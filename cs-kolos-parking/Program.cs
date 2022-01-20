3e9f14116f7aa1f8c848fdebc88e95b5709953ae
Sugar <sugarypolecat@gmail.com>
2022-01-20 20:57:20 +0100

    Add project files.

---
+++ cs-kolos-parking/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_kolos_parking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
